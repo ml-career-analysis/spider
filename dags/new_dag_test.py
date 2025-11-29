5ff5c0ec58bf46a29552904a65df85dd6db5a9bd
hollenhaunf <rkgaming248@gmail.com>
2025-11-29 22:37:04 +0300

    pull_new_test

---
+++ dags/new_dag_test.py
@@ -0,0 +1,15 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    'git_pull_testi4',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    hello_task = BashOperator(
+        task_id='hello_task',
+        bash_command='echo "Hello World"',
+    )
