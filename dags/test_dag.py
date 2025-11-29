69d005212b65c7919e27a6b0b3c70674d7821c52
hollenhaunf <rkgaming248@gmail.com>
2025-11-29 23:03:22 +0300

    test

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -1,8 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
-# test
-# new test
+
 with DAG(
     'simple_hello',
     start_date=datetime(2024, 1, 1),
