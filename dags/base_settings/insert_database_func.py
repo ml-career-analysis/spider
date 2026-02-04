e97868b2739d032007a0652d90c9a9f3c7fedefe
hollenhaunf <rkgaming248@gmail.com>
2026-02-04 17:01:43 +0300

    upgraded_insert

--- dags/base_settings/insert_database_func.py
+++ dags/base_settings/insert_database_func.py
@@ -1,5 +1,9 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from psycopg2.extras import execute_values
 import pandas as pd
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+import ast
+from contextlib import closing
+import logging
 
 def push_df_to_db(
     ti,
@@ -13,9 +17,31 @@ def push_df_to_db(
 ):
     filename = ti.xcom_pull(task_ids=task_id_xcom, key=key_xcom)
     df = pd.read_csv(filename)
-    print(df)
-    pg_conn = PostgresHook(postgres_conn_id = 'arxiv')
-    q = """select * from articles limit 10"""
-    df = pg_conn.get_pandas_df(q)
-    print(df)
+    logging.info(df)
+    df["id"] = df["id"].astype(str)
+
+    columns = list(df.columns)
+    records = df.to_records(index=False).tolist()
+
+    insert_cols = ", ".join(columns)
+    conflict_cols = ", ".join(index_cols)
+
+    update_set = ", ".join(
+        f"{col} = EXCLUDED.{col}" for col in update_cols
+    )
+
+    sql = f"""
+        INSERT INTO {schema_name}.{table_name} ({insert_cols})
+        VALUES %s
+        ON CONFLICT ({conflict_cols})
+        DO UPDATE SET
+        {update_set}
+    """
+
+    pg_hook = PostgresHook(postgres_conn_id=conn_id)
+
+    with closing(pg_hook.get_conn()) as conn:
+        with conn.cursor() as cur:
+            execute_values(cur, sql, records, page_size=1000)
+            conn.commit()
 
