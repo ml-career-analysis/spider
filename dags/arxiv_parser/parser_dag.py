e3c21bcdf87d61a834c659409da7cd08dbbc69b7
hollenhaunf <rkgaming248@gmail.com>
2026-02-06 22:03:06 +0300

    trigger_test

--- dags/arxiv_parser/parser_dag.py
+++ dags/arxiv_parser/parser_dag.py
@@ -224,14 +224,14 @@ with DAG(
             "index_cols": ["id"],
         }
     )
-    # trigger_betting_dag = TriggerDagRunOperator(
-    #     task_id="trigger_section_processing_dag",
-    #     trigger_dag_id="sectioned_text",
-    #     wait_for_completion=False,  
-    #     poke_interval=5,  
-    #     conf={
-    #         "message": "Data from upstream DAG"
-    #     },  
-    # )
-    fetch_scraper >> fetch_arxiv >> test >> insert #>> trigger_betting_dag
+    trigger_betting_dag = TriggerDagRunOperator(
+        task_id="trigger_section_processing_dag",
+        trigger_dag_id="sectioned_text",
+        wait_for_completion=False,  
+        poke_interval=5,  
+        conf={
+            "message": "Data from upstream DAG"
+        },  
+    )
+    fetch_scraper >> fetch_arxiv >> test >> insert >> trigger_betting_dag
 
