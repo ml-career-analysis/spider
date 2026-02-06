55ad8560044aec443fb28d4e7c7db5d4e751a58e
hollenhaunf <rkgaming248@gmail.com>
2026-02-06 21:31:43 +0300

    test

--- dags/arxiv_parser/section_parser.py
+++ dags/arxiv_parser/section_parser.py
@@ -21,7 +21,7 @@ select
     pdf_url
 from articles a
 where a.clean_text is not null and section_text_new is null
-limit 1
+limit 100
 """
 # пока что поставил лимит, пока сам не перелью базу в секцию иначе будет фечить все 
 
